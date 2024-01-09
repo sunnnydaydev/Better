e5215e622348b5011450838303e6c0f3f590516a
SunnyDay <1246771571@qq.com>
2024-01-09 22:00:38 +0800

    base fragment 抽取
    todo 测试base fragment

---
+++ app/src/main/java/com/example/better/core/BaseFragment.kt
@@ -0,0 +1,92 @@
+package com.example.better.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+/**
+ * Create by SunnyDay /01/09 21:03:24
+ */
+abstract class BaseFragment<VS, VE> : Fragment() {
+    protected abstract val binding: ViewDataBinding
+    protected abstract val viewModel: BaseViewModel<VS, VE>
+    protected open val rootView: View
+        get() = view ?: throw IllegalStateException("View not inflated yet")
+
+    final override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Timber.d("my-test1")
+        return binding.root
+    }
+
+    /**
+     * as api said this method is invoked after onCreateView
+     * */
+    final override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Timber.d("my-test2")
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.uiFlow.collectLatest {
+                        onViewStateUpdate(it)
+                    }
+                }
+                launch {
+                    viewModel.actionFlow.collect {
+                        when (it) {
+                            is ViewAction.DisplayScreen<*> -> onDisplayScreenAction(it)
+                            is ViewAction.CloseScreen -> activity?.finish()// navigation up
+                            is ViewAction.ShowToast -> showToast(it.msg)
+                        }
+                    }
+                }
+            }
+        }
+        initView()
+    }
+
+    final override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.d("my-test3")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("my-test4")
+    }
+
+    override fun onDestroyView() {
+        (rootView.parent as? ViewGroup)?.endViewTransition(rootView)
+        super.onDestroyView()
+    }
+
+    abstract fun initView()
+
+    /**
+     * 最新的UI数据
+     * */
+    abstract fun onViewStateUpdate(viewState: VS)
+
+    /**
+     * UI层收到action处理工作。这里主要处理DisplayScreen相关操作。如弹窗，跳页面。
+     * */
+    abstract fun onDisplayScreenAction(it: ViewAction.DisplayScreen<*>)
+
+    private fun showToast(msg: String) {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/better/ui/MainActivity.kt
+++ app/src/main/java/com/example/better/ui/MainActivity.kt
@@ -10,24 +10,16 @@ import com.example.better.utils.BindActivity
 import com.example.better.vms.MainViewModel
 import timber.log.Timber
 
-class MainActivity : BaseActivity<ViewState,ViewEvent>() {
+class MainActivity : BaseActivity<ViewState, ViewEvent>() {
 
-    override val mBinding: ActivityMainBinding  by BindActivity(R.layout.activity_main)
+    override val mBinding: ActivityMainBinding by BindActivity(R.layout.activity_main)
 
     // 这里不通过viewModelFactory创建了。通过viewModelFactory方式创建可以自己手动控制MainViewModel的创建方式，如构造注入字段。
     // 支持Hilt注入则没必要这样搞了。
-    override val mViewModel  by viewModels<MainViewModel>()
+    override val mViewModel by viewModels<MainViewModel>()
 
 
-    override fun initView() {
-      mBinding.tvBtn.setOnClickListener {
-          dispatchEvent(ViewEvent.Init)
-      }
-
-        mBinding.tvBtn2.setOnClickListener {
-            dispatchEvent(ViewEvent.Test)
-        }
-    }
+    override fun initView() {}
 
     override fun onDisplayScreenAction(it: ViewAction.DisplayScreen<*>) {
 
